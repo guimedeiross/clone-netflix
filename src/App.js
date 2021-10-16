26fad6524f8ffe26ff3e5b92501650f54f690c11
Guilherme Schubert Medeiros <guilhermeschu@gmail.com>
2021-10-16 12:51:14 -0300

    Projeto finalizado, reponsividade funcionando

--- src/App.js
+++ src/App.js
@@ -17,18 +17,18 @@ export default () => {
       setMovieList(list)
 
       let originals = list.filter(i => i.slug === 'originals')
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1) )
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
       let chosen = originals[0].items.results[randomChosen]
       let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
       setFeaturedData(chosenInfo)
     }
-    
+
     loadAll()
   }, [])
 
-  useEffect(()=>{
+  useEffect(() => {
     const scrollListener = () => {
-      if(window.scrollY > 10) {
+      if (window.scrollY > 10) {
         setBlackHeader(true)
       } else {
         setBlackHeader(false)
@@ -38,7 +38,7 @@ export default () => {
     window.addEventListener('scroll', scrollListener)
 
     return () => {
-      window.removeEventListener('scroll', scrollListener) 
+      window.removeEventListener('scroll', scrollListener)
     }
   }, [])
   return (
@@ -53,10 +53,15 @@ export default () => {
         ))}
       </section>
       <footer>
-        Desenvolvido com <span role="img" aria-label="coração">❤️</span> por Guilherme Schubert medeiros<br/>
-        Direitos de imagem para Netflix<br/>
+        Desenvolvido com <span role="img" aria-label="coração">❤️</span> por Guilherme Schubert Medeiros<br />
+        Direitos de imagem para Netflix<br />
         Dados obtidos do site Themoviedb.org
       </footer>
+      {movieList <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando"></img>
+        </div>
+      }
     </div>
   )
 }
